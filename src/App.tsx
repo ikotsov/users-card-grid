96f722c3002cba9cbbd5100af3f666b8bab032b4
ikotsov <i.kotsovilis@gmail.com>
2022-03-14 18:51:36 +0200

    refactor: remove initial styles and html

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Component, createResource } from "solid-js";
 import styles from "./App.module.css";
 import { fetchUsers } from "./fetchUsers";
-import logo from "./logo.svg";
-import { Name } from "./User/Name";
 
 const App: Component = () => {
   const [users] = createResource(5, fetchUsers);
@@ -11,23 +9,8 @@ const App: Component = () => {
     <div class={styles.App}>
       <span>{users.loading && "Loading..."}</span>
       <div>
-        <pre>{JSON.stringify(users(), null, 2)}</pre>
+        <pre style="margin: 0">{JSON.stringify(users(), null, 2)}</pre>
       </div>
-      <Name>John</Name>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
     </div>
   );
 };
