56d0c99066817e9b5404d87096698eaf5d42a933
ikotsov <i.kotsovilis@gmail.com>
2022-04-19 01:36:19 +0300

    refactor: access the correct property from response

--- src/fetchUsers.ts
+++ src/fetchUsers.ts
@@ -1,4 +1,8 @@
 const MAIN_URL = "https://randomuser.me/api";
 
-export const fetchUsers = async (total: number) =>
-  (await fetch(`${MAIN_URL}/?results=${total}`)).json();
+export const fetchUsers = async (total: number) => {
+  const response = await fetch(`${MAIN_URL}/?results=${total}`);
+  const json = await response.json();
+  console.log(json);
+  return json.results;
+};
