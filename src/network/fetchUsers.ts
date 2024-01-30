102988ae5817c0bbf2b1d0208c39e0240f2e2ae0
ikotsov <i.kotsovilis@gmail.com>
2024-01-31 01:13:32 +0200

    refactor: to filet users

--- src/network/fetchUsers.ts
+++ src/network/fetchUsers.ts
@@ -1,8 +1,10 @@
+import { isUser } from "./validation";
+
 const MAIN_URL = "https://randomuser.me/api";
 
 export const fetchUsers = async (total: number) => {
   const response = await fetch(`${MAIN_URL}/?results=${total}`);
   const json = await response.json();
-  console.log(json);
-  return json.results;
+  console.log(json.results.filter(isUser));
+  return json.results.filter(isUser);
 };
